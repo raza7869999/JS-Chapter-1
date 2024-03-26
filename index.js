ff008cf978b87dde0e64b043a952d5a933fac308
Muhammad Hasnain Raza <157500053+raza7869999@users.noreply.github.com>
2024-03-26 10:44:19 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+//Q:1
+alert()
+
+//Q:2
+
+alert("Error! Please enter a Valid Password");
+
+//Q:3
+
+alert("Welcome to JS Land... \nHappy Coding!" )
+
+
+//Q:5
+
+alert ("Hello...!I can run JS through my web browser's console")
+
+
+
+  
